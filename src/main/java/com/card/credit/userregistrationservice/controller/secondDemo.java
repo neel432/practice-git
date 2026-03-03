ad90e6b7629bced2761605423abb782cc03d418c
neel432 <134537234+neel432@users.noreply.github.com>
2026-03-03 09:16:41 +0530

    Commit for second demo file

---
+++ src/main/java/com/card/credit/userregistrationservice/controller/secondDemo.java
@@ -0,0 +1,8 @@
+package com.card.credit.userregistrationservice.controller;
+
+public class secondDemo {
+
+     public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
